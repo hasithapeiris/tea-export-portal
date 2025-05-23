b3b264ce370066b64570c13daf360c80b6777b22
Hasitha <hasithapeiris.me@gmail.com>
2025-05-23 23:42:40 +0530

    fix cookie issue

--- main-server/utils/setTokenCookie.js
+++ main-server/utils/setTokenCookie.js
@@ -2,7 +2,7 @@ export const setTokenCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
     maxAge: 60 * 60 * 1000, // 1 hour
   });
 };
