a09065a343d7d92394eed4051ec6be85f85c346e
Hasitha <hasithapeiris.me@gmail.com>
2025-05-26 12:14:28 +0530

    fix: fix price prediction service

--- price-ml-service/main.py
+++ price-ml-service/main.py
@@ -1,16 +1,21 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from controller import router as tea_prediction_router
 
-
 app = FastAPI()
+
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins=["*"] ,
+    CORSMiddleware,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"] 
-
+    allow_headers=["*"]
 )
 
-app.include_router(tea_prediction_router , prefix="/tea-project")
+app.include_router(tea_prediction_router, prefix="/tea-project")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
